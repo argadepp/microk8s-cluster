cdf92711d22fb47fd6ed056d0cbb7789484973c9
pratikargade <pratik.argade@coditas.com>
2023-09-01 19:25:19 +0530

    ver changed

--- cluster/provider.tf
+++ cluster/provider.tf
@@ -3,14 +3,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.27.0"
+
     }
   }
 
-required_version = "1.4.0"
 }
 
 provider "aws" {
   region  = "ap-south-1"
-        version = "~>3.27.0"
+
 }
