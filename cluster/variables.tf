57eb329b3ead7584426df6efc49ad3cdbc6c3462
pratikargade <pratik.argade@coditas.com>
2023-09-02 20:51:08 +0530

    spell mistake

--- cluster/variables.tf
+++ cluster/variables.tf
@@ -1,9 +1,12 @@
 variable "instType" {
   type = string
+  default = "t2.medium"
 }
 variable "product" {
   type = string
+  default = "DevOps"
 }
 variable "env" {
   type = string
+  default = "dev"
 }
