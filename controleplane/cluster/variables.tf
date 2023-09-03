6391df2ff65669215c2c33db0a6fdf701c66bcf2
pratikargade <pratik.argade@coditas.com>
2023-09-03 15:34:22 +0530

    new controleplane

--- controleplane/cluster/variables.tf
+++ controleplane/cluster/variables.tf
@@ -2,3 +2,17 @@
 variable "regiom" {
   type = string
 }
+variable "product" {
+ type = string  
+ default = "DevOpsTechs"
+}
+
+variable "env" {
+  type = string
+  default = "dev"  
+}
+
+variable "instType" {
+    type = string
+    default = "t2.medium"
+}
