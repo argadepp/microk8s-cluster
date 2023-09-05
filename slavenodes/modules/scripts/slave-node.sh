08c10825ec9cccd33525f7d397b43d865c22a2d5
pratikargade <pratik.argade@coditas.com>
2023-09-05 17:35:51 +0530

    added slavenodes

--- slavenodes/modules/scripts/slave-node.sh
+++ slavenodes/modules/scripts/slave-node.sh
@@ -10,7 +10,7 @@ sudo userdmod -aG docker ubuntu
 
 sudo snap install microk8s --classic --channel 1.22-eksd/stable
 
-sudo 
+sudo microk8s join 172.31.0.42:25000/81affa0eeac5b2a5740fcca60b538c31/6db437d4203e
  
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
