b81b01d937b33365e9b8260baddc64c0353641a5
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2023-09-24 19:19:46 +0530

    Update slave-node.sh

--- slavenodes/modules/scripts/slave-node.sh
+++ slavenodes/modules/scripts/slave-node.sh
@@ -10,7 +10,7 @@ sudo userdmod -aG docker ubuntu
 
 sudo snap install microk8s --classic --channel 1.22-eksd/stable
 
-microk8s join 172.31.35.99:25000/2c70e3a8552915cd39e8e51279fb065c/da3a950f3443 --worker
+sudo microk8s join 172.31.35.99:25000/2c70e3a8552915cd39e8e51279fb065c/da3a950f3443 --worker
  
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
