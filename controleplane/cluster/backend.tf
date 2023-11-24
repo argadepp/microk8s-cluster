b4da01d70ce73f1a9e78145e908a5eca5d44b321
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2023-11-24 12:47:17 +0530

    Update backend.tf

--- controleplane/cluster/backend.tf
+++ controleplane/cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-backend-pratik"
-    key    = "cluster/worker1.tfstate"
+    key    = "cluster/worker2.tfstate"
     region = "ap-south-1"
   }
 }
